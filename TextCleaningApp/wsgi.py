9058c142e70a5f4f67aa9ed9ba2774fee1dfd75f
amarendra234 <39214646+amarendra234@users.noreply.github.com>
2024-11-10 17:12:03 +0530

    Update wsgi.py

--- TextCleaningApp/wsgi.py
+++ TextCleaningApp/wsgi.py
@@ -1,1 +1,9 @@
+"""
+WSGI config for TextCleaningApp project.
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TextCleaningApp.settings')
+application = get_wsgi_application()
 
